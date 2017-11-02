7ec0e5dd2ecb095b5e1cea67c9c4873e01d1b926
Tomeu Canyelles Escarrer <bcanyelles@apsl.net>
2017-11-02 16:51:56 +0100

    Update webhooks messaging * Telegram * Slack

--- alerta/app/webhooks/parsers.py
+++ alerta/app/webhooks/parsers.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from alerta.app import app, db
 from alerta.app.utils import absolute_url
@@ -13,6 +14,9 @@ class ConfigParser(object):
         super(ConfigParser, self).__init__()
 
     def get_customer_config(self, customer="default"):
+        """
+        Get customer config file.
+        """
         try:
             return self._get_config(customer=customer)
         except Exception as e:
@@ -24,6 +28,9 @@ class ConfigParser(object):
             return Config('/')
 
     def _get_config(self, customer="default"):
+        """
+        Get customer or default config file according customer name.
+        """
         config, customer = Config('/'), "_{}".format(customer)
         try:
             loaded = config.from_pyfile("/etc/alertad{}.conf".format(customer), silent=False)
@@ -38,6 +45,9 @@ class ConfigParser(object):
         return config
 
     def get_default_config(self):
+        """
+        Get application config file.
+        """
         from alerta.app import app
         return app.config
 
@@ -55,6 +65,9 @@ class SlackParser(ConfigParser):
     }
 
     def parse_slack(self, data):
+        """
+        Parser request data to get `alert`, `user` and `action`.
+        """
         payload = json.loads(data['payload'])
 
         user = payload.get('user', {}).get('name')
@@ -75,6 +88,9 @@ class SlackParser(ConfigParser):
         return alert, user, action
 
     def build_slack_response(self, alert, action, user, data):
+        """
+        Send the response message and sent request to add it.
+        """
         response = json.loads(data['payload']).get('original_message', {})
         actions = ['open', 'ack', 'close']
 
@@ -118,6 +134,10 @@ class TelegramParser(ConfigParser):
                          u"{alert} of {customer} customer.",
         'loading_error': u"Error getting configuration by {customer} customer.\n Telegram reply for {action} on "
                          u"{alert} won't be sent.",
+        'message_format': u"{alarm} `{level}` - *{event}*\n"
+                          u"`Resource: {resource}`\n`Customer: {customer}`\n`Severity: {severity}`\n"
+                          u">> {text}",
+        'extra_message_format': u"{log}\n{reply}",
         'reply_message': u"\u2713 Alert {alert} is *{status}* now! Action done by {user}.",
         'reply_watching_message': u"\u23F5 User {user} is watching alert {alert}.",
         'sending_error': u"Error sending reply message for {action} on {alert}."
@@ -125,9 +145,15 @@ class TelegramParser(ConfigParser):
 
     @staticmethod
     def valid_data(data):
-        return 'callback_query' in data
+        """
+        Validate id `callback_query` is in data dict
+        """
+        return data and 'callback_query' in data
 
     def parser_telegram(self, data):
+        """
+        Parser request data to get `alert`, `user` and `action`.
+        """
         author = data['callback_query']['from']
         command, alert = data['callback_query']['data'].split(' ', 1)
         action = command.lstrip('/')
@@ -138,7 +164,52 @@ class TelegramParser(ConfigParser):
 
         return alert, user, action
 
+    def _chat_id(self, data):
+        """
+        The channel id for response. Get it from request message
+        """
+        return data['callback_query']['message']['chat']['id']
+
+    def _message_log(self, data):
+        """
+        Parser text message text to find the original message.
+        Try to find the text between '>>' and '\n' characters as end of original message.
+        """
+        message = data['callback_query']['message']['text']
+        search = re.search(">>\s*(?P<text>(?!\n).*)", message)
+        if not search:
+            return message
+        text = search.group('text')
+        message = message[message.index(text) + len(text):]
+        for char in ('\n', ' '):
+            message = message.strip(char)
+        message = u"\n{}".format(message) if message else message
+        return message
+
+    def _reply_message(self, action, alert, user, config):
+        """
+        Get the format of the reply message.
+        It can be configured for each customer in their config files.
+        """
+        reply = config.get('TELEGRAM_REPLY_FORMAT', self.messages['reply_message'])
+        if action == 'watch':
+            reply = config.get('TELEGRAM_REPLY_WATCHING_FORMAT', self.messages['reply_watching_message'])
+        reply = unicode(reply).format(alert=alert, status=action, user=user)
+        return reply
+
+    def _get_message_format(self, config):
+        """
+        Recovery the format of original message to keep it when response (update it).
+        It can be configured for each customer in their config files.
+        """
+        msg = config.get('TELEGRAM_MESSAGE_FORMAT', self.messages['message_format'])
+        extra_msg = config.get('TELEGRAM_EXTRA_MESSAGE_FORMAT', self.messages['extra_message_format'])
+        return u"{}{}".format(unicode(msg), unicode(extra_msg))
+
     def send_message_reply(self, alert_id, action, user, data):
+        """
+        Send the response message and sent request to update it.
+        """
         try:
             import telepot
         except ImportError as e:
@@ -152,17 +223,14 @@ class TelegramParser(ConfigParser):
         inline_keyboard = []
         params = {'action': action, 'alert': alert_short_id, 'customer': alert.customer}
 
-        # default_reply_message = u""
-        # default_reply_watching_message = u""
-
         try:
             customer_config = self.get_customer_config(alert.customer)
             config = self.get_default_config()
 
             token = customer_config.get('TELEGRAM_TOKEN')
-            chat_id = customer_config.get('TELEGRAM_CHAT_ID')
             dashboard_url = config.get('DASHBOARD_URL')
 
+            chat_id = self._chat_id(data)
             if not (token and dashboard_url and chat_id):
                 raise ValueError(self.messages['data_expected'].format(**params))
         except Exception as e:
@@ -172,12 +240,10 @@ class TelegramParser(ConfigParser):
         try:
             # message info
             message_id = data['callback_query']['message']['message_id']
-            message_log = "\n".join(data['callback_query']['message']['text'].split('\n')[1:])
-            reply = unicode(customer_config.get('TELEGRAM_REPLY_FORMAT', self.messages['reply_message']))
+            message_log = self._message_log(data)
+            message_format = self._get_message_format(customer_config)
 
             if action == 'watch':
-                reply = unicode(customer_config.get('TELEGRAM_REPLY_WATCHING_FORMAT',
-                                                    self.messages['reply_watching_message']))
                 inline_keyboard = [
                     [{'text': 'Watch', 'callback_data': "{} {}".format('/watch', alert_id)},
                      {'text': 'Ack', 'callback_data': "{} {}".format('/ack', alert_id)},
@@ -185,11 +251,15 @@ class TelegramParser(ConfigParser):
                 ]
 
             # format message response
+            text = alert.text.replace('_', '\_')
             alert_url = "{}/#/alert/{}".format(dashboard_url, alert.id)
-            reply = reply.format(alert=alert_short_id, status=action, user=user)
-            message = u"{alert} `{level}` - *{event} on {resouce}*\n{log}\n{reply}".format(
-                alert="[{}]({})".format(alert_short_id, alert_url), level=alert.severity.upper(),
-                event=alert.event, resouce=alert.resource, log=message_log, reply=reply)
+            reply = self._reply_message(action, alert_short_id, user, customer_config)
+            severity = u"{} \u2192 {}".format(alert.previous_severity or 'indeterminate', alert.severity)
+            message = message_format.format(
+                alarm="[{}]({})".format(alert_short_id, alert_url), level=alert.severity.upper(), event=alert.event,
+                resource=alert.resource, customer=alert.customer, severity=severity, text=text, log=message_log,
+                reply=reply
+            )
 
             # send message
             bot = telepot.Bot(token)
